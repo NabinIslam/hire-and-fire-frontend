df7cf73e4ae5a856a0e6e83c087b0ad91938c20a
NabinIslam <sinabin62246@gmail.com>
2024-08-29 12:53:22 +0600

    fix

--- src/Utils/capitalizeAndRemoveHyphens.js
+++ src/Utils/capitalizeAndRemoveHyphens.js
@@ -1,8 +1,6 @@
 export function capitalizeAndRemoveHyphens(str) {
-  // Replace hyphens with spaces
   const replacedString = str.replace(/-/g, " ");
 
-  // Capitalize the first letter of each word
   const capitalizedString = replacedString
     .split(" ")
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
