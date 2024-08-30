bf7f79807dc42e12bc44abd33a7ad43ad3bc8e8b
NabinIslam <sinabin62246@gmail.com>
2024-08-30 18:58:16 +0600

    card loading

--- src/app/industry/page.jsx
+++ src/app/industry/page.jsx
@@ -2,7 +2,7 @@ import IndustryCard from "@/components/IndustryCard";
 import PageBanner from "@/components/PageBanner";
 import { apiBaseUrl } from "@/secrets";
 import { Suspense } from "react";
-import IndustrySkeleton from "./loading";
+import Skeleton from "react-loading-skeleton";
 
 const Industry = async () => {
   const data = await fetch(`${apiBaseUrl}/industries`, {
@@ -22,13 +22,26 @@ const Industry = async () => {
           <div className="mx-auto h-[5px] w-[200px] rounded-full bg-primary"></div>
 
           {/* industries container  */}
-          <Suspense fallback={<IndustrySkeleton />}>
-            <div className="mt-10 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-              {industries.map((industry) => (
+
+          <div className="mt-10 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {industries.map((industry) => (
+              <Suspense
+                fallback={
+                  <div className="rounded-lg border p-4 text-center shadow-xl">
+                    <Skeleton className="h-[240px] w-full" />
+
+                    <Skeleton className="h-[20px]" />
+                    <Skeleton className="h-[10px]" />
+                    <Skeleton className="h-[10px]" />
+                    <Skeleton className="h-[10px]" />
+                    <Skeleton className="h-[15px] w-[88px]" />
+                  </div>
+                }
+              >
                 <IndustryCard industry={industry} key={industry.name} />
-              ))}
-            </div>
-          </Suspense>
+              </Suspense>
+            ))}
+          </div>
         </div>
       </section>
     </main>
