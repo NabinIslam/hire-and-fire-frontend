6f62c957cc194dac7f420f218e37caa9be4d5cf2
NabinIslam <sinabin62246@gmail.com>
2024-08-30 20:12:22 +0600

    push

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import Button from "./ui/Button";
 import { IoMdArrowDropright } from "react-icons/io";
 import { FaPhone, FaEnvelope } from "react-icons/fa6";
 import {
