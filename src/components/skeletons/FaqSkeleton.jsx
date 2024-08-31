794505a2e9781d34908da0667c01a9168df99da1
NabinIslam <sinabin62246@gmail.com>
2024-08-31 12:55:30 +0600

    space

--- src/components/skeletons/FaqSkeleton.jsx
+++ src/components/skeletons/FaqSkeleton.jsx
@@ -2,7 +2,7 @@ import Skeleton from "react-loading-skeleton";
 
 const FaqSkeleton = () => {
   return (
-    <div className="space-y-4">
+    <div className="space-y-10">
       <Skeleton className="h-[54px] w-full" />
       <Skeleton className="h-[54px] w-full" />
       <Skeleton className="h-[54px] w-full" />
