c3451280d411db54e5c683a29e0691f708449517
NabinIslam <sinabin62246@gmail.com>
2024-09-02 15:28:06 +0600

    suspense wrap

--- src/app/industry/page.jsx
+++ src/app/industry/page.jsx
@@ -1,6 +1,8 @@
 import IndustryCard from "@/components/IndustryCard";
 import PageBanner from "@/components/PageBanner";
 import { apiBaseUrl } from "@/secrets";
+import { Suspense } from "react";
+import Loading from "./loading";
 
 const Industry = async () => {
   const data = await fetch(`${apiBaseUrl}/industries`, {
@@ -9,26 +11,28 @@ const Industry = async () => {
   const industries = await data.json();
 
   return (
-    <main>
-      <PageBanner title="Industry" />
+    <Suspense fallback={<Loading />}>
+      <main>
+        <PageBanner title="Industry" />
 
-      <section className="py-[100px]">
-        <div className="container">
-          <h2 className="mb-5 text-center text-5xl font-semibold">
-            Industry We Serve
-          </h2>
-          <div className="mx-auto h-[5px] w-[200px] rounded-full bg-primary"></div>
+        <section className="py-[100px]">
+          <div className="container">
+            <h2 className="mb-5 text-center text-5xl font-semibold">
+              Industry We Serve
+            </h2>
+            <div className="mx-auto h-[5px] w-[200px] rounded-full bg-primary"></div>
 
-          {/* industries container  */}
+            {/* industries container  */}
 
-          <div className="mt-10 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {industries.map((industry) => (
-              <IndustryCard industry={industry} key={industry.name} />
-            ))}
+            <div className="mt-10 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+              {industries.map((industry) => (
+                <IndustryCard industry={industry} key={industry.name} />
+              ))}
+            </div>
           </div>
-        </div>
-      </section>
-    </main>
+        </section>
+      </main>
+    </Suspense>
   );
 };
 
