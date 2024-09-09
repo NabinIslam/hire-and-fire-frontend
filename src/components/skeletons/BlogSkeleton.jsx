90f9afabc4fbed36359c0d2335114de3265fccee
NabinIslam <sinabin62246@gmail.com>
2024-09-09 11:48:18 +0600

    added 2 more blog skeleton card

--- src/components/skeletons/BlogSkeleton.jsx
+++ src/components/skeletons/BlogSkeleton.jsx
@@ -64,6 +64,26 @@ const BlogSkeleton = () => {
         <Skeleton className="h-[10px]" />
         <Skeleton className="h-[15px] w-[88px]" />
       </div>
+      {/* card  */}
+      <div className="rounded-lg border p-4 text-center shadow-xl">
+        <Skeleton className="h-[240px] w-full" />
+
+        <Skeleton className="mt-4 h-[20px]" />
+        <Skeleton className="h-[10px]" />
+        <Skeleton className="h-[10px]" />
+        <Skeleton className="h-[10px]" />
+        <Skeleton className="h-[15px] w-[88px]" />
+      </div>
+      {/* card  */}
+      <div className="rounded-lg border p-4 text-center shadow-xl">
+        <Skeleton className="h-[240px] w-full" />
+
+        <Skeleton className="mt-4 h-[20px]" />
+        <Skeleton className="h-[10px]" />
+        <Skeleton className="h-[10px]" />
+        <Skeleton className="h-[10px]" />
+        <Skeleton className="h-[15px] w-[88px]" />
+      </div>
     </div>
   );
 };
