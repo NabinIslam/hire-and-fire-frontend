03d0b1dbb91d32aa86a28bf93ad6c13102c467e9
Nabin Islam <sinabin62246@gmail.com>
2024-09-26 17:16:40 +0600

    industry card updated

--- src/components/pages/industry/IndustryCard.jsx
+++ src/components/pages/industry/IndustryCard.jsx
@@ -7,9 +7,10 @@ const IndustryCard = ({ industry }) => {
   const { name, shortDescription, photo, slug } = industry;
 
   return (
-    <FadeInLeftWithSlowBounce>
-      <div className="flex flex-col items-center rounded-lg border-b-8 border-b-primary bg-industry-card-bg bg-contain bg-no-repeat p-4 text-center shadow-xl">
-        <div className="relative min-h-[240px] w-full">
+    <FadeInLeftWithSlowBounce className="group relative">
+      <div className="absolute h-[150px] w-full rounded-t-lg rounded-bl-3xl rounded-br-3xl bg-primary duration-200 group-hover:bg-secondary"></div>
+      <div className="flex flex-col items-center rounded-lg border-b-8 border-b-primary p-4 text-center shadow-xl duration-200 group-hover:border-b-secondary">
+        <div className="relative min-h-[240px] w-full rounded-b-full">
           <Image
             className="rounded-[33px] object-cover"
             src={photo}
